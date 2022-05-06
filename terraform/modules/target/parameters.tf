550a68b5cd4520216aeab31136c0c0aa41cdc05d
jimatomo <iitomoamuz@gmail.com>
2022-05-06 20:42:45 +0900

    [bugfix] rds password

--- terraform/modules/target/parameters.tf
+++ terraform/modules/target/parameters.tf
@@ -2,17 +2,11 @@
 # SSM Parameter Store
 #-----------------------------------------
 
-resource "random_password" "rds_instance_password_001" {
-  length           = 16
-  special          = true
-  override_special = "!#%&*()-_=+[]{}<>?"
-}
-
 resource "aws_ssm_parameter" "rds_instance_password" {
   name        = var.paramter_store_name_rds_instance_password
   description = "RDS mastasr user password"
   type        = "SecureString"
-  value       = random_password.rds_instance_password_001.result
+  value       = random_password.rds_instance_password.result
 }
 
 resource "aws_ssm_parameter" "secret" {
